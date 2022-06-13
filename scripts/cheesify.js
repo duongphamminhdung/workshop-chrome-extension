1df0b83c7548bce29176c7636d39291ec4f9674e
Duong Pham Minh Dung <duongphamminhdung@gmail.con>
2022-06-13 12:32:12 +0700

    testing "search by tagname"

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,6 +1,24 @@
-function cheesify() {
-  // TODO: Add the image replacement script here
+function cheesify(tagname) {
+  let Index = '';
+  const tag = tagname;
+  const collection = document.getElementsByTagName(tag);
+  for (let i = 0; i < collection.length; i++) {
+    temp = document.getElementsByTagName(tag)[i].innerHTML
+    console.log(temp)
+    if (temp.includes('<') ) {
+      continue;
+    }
+    Index = Index + temp + "\n";
+  }
+  console.log(Index);
+  alert("Open console to see the result");
+
 }
 
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-  
+  // Listen for messages on the content page
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.action === 'cheesify') cheesify(request.TAGNAME);
+  }
+);
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,14 @@
-function sendCheesifyMsg() {
-  // TODO: Write a function to send a message to the active tab to 'cheesify' it
+// scripts/popup.js
+
+// Send a message to the active tab to 'cheesify' it
+function sendCheesifyMsg(tagname) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { // Finds tabs that are active in the current window
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'cheesify', TAGNAME : tagname}); // Sends a message (object) to the first tab (tabs[0])
+  });
 }
+
+// Trigger the function above when clicking the 'Cheesify' button
+var tagname = document.getElementById("input1").value;
+document.querySelector('#cheesify').addEventListener('click', event => sendCheesifyMsg(tagname));
 
 // TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
