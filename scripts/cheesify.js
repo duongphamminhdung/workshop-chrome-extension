5e5d4cdd0c8be6b12aa65e74a2aee710aa694595
Duong Pham Minh Dung <duongphamminhdung@gmail.com>
2022-06-14 21:58:13 +0700

    change milkify
    asd

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,34 +1,44 @@
 function cheesify(tagname, food) {
-    let Index = '';
-    let count = 10;
-    const tag = tagname;
-    console.log(tag)
-    const collection = document.getElementsByTagName(tag);
-    for (let i = 0; i < collection.length; i++) {
-      temp = collection[i].innerHTML;
-      console.log(temp);
       if (food === 'cheese') {
-        if (temp.includes('<') ) {
+        let Index = '';
+        const tag = tagname;
+        console.log(tag)
+        const collection = document.getElementsByTagName(tag);
+        for (let i = 0; i < collection.length; i++) {
+          temp = collection[i].innerHTML;
+          console.log(temp);
+          if (temp.includes('<') ) {
           continue;
+          }
+          Index = Index + temp + "\n";
         }
-        Index = Index + temp + "\n";
+        //inject paragraph of pages that was translated direcly
       }
-      //inject paragraph of pages that was translated direcly
+
+
       else if (food === 'milk') {
-        if (count == 0) {
-          Index = Index + "\n" + "\n";
-          count = 10;
+        contentBox = document.getElementsByClassName("contentbox")[1].innerText;
+        Index = '';
+        count = 10;
+        for (let i = 0; i < contentBox.length; i++) {
+          if (contentBox[i] == "\ ".substring(0, 10) && contentBox[i+1] == "n") {
+            contentBox = contentBox.replace(contentBox[i], ' ');
+          }
+          if (contentBox[i] === '.') {
+            count -= 1;
+          }
+          if (count == 0){
+            Index = Index + "\n";
+            count = 10;
+          }
         }
-        Index = Index + temp + " ";
-        count = count - 1;
-      }
       //inject paragraph that was translated by js in stv
     }
     
     console.log(Index);
     alert("Open console to see the result");
-}
 
+  }
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
   // Listen for messages on the content page
 chrome.runtime.onMessage.addListener(
